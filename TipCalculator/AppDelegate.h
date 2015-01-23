a9b494de1cc50b3a6cf28e8745e1c3b8659bfe84
tgulik <tgulik@yahoo-inc.com>
2015-01-22 17:48:09 -0800

    Adding tipCalc
    Adding tipCalculator

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Tom Gulik on 6/4/14.
+//  Copyright (c) 2014 Tom Gulik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
